49c11fc32ccf879c0cb4cb2b1b149db3ae9af126
Samyak Jain <samtan106@gmail.com>
2020-03-17 20:55:55 +0530

    Changed url to fix iframe issue

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ app.use(cors(corsOptions));
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/do', (req, res) => {
-    new inliner('https://view.officeapps.live.com/op/embed.aspx?src=' + req.query.url, (error, html) => {
+    const url = `https://US6-word-view.officeapps.live.com/wv/wordviewerframe.aspx?embed=1&ui=en%2DUS
+    &rs=en%2DUS&WOPISrc=http%3A%2F%2Fus6%2Dview%2Dwopi%2Ewopi%2Elive%2Enet%3A808%2Foh%2Fwopi%2Ffiles%
+    2F%40%2FwFileId%3FwFileId%3D${req.query.url}&access_token_ttl=0`
+
+    new inliner(url, (error, html) => {
         res.send(html)
     })
 })

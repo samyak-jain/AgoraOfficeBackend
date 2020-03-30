016016565efbac579b64f74467743e0b3772f375
Samyak Jain <samtan106@gmail.com>
2020-03-30 07:31:41 +0530

    File upload integration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const request = require('request');
 const httpProxy = require('http-proxy');
+const fileUpload = require('express-fileupload');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,41 +13,58 @@ const corsOptions = {
 
 const proxy = httpProxy.createProxyServer({});
 
-proxy.on("proxyReq", (proxyReq, req, res, options) => {
-    const url = "US6-word-view.officeapps.live.com";
+proxy.on('proxyReq', (proxyReq, req, res, options) => {
+    const url = 'US6-word-view.officeapps.live.com';
     proxyReq.setHeader('origin', `https://${url}`);
     proxyReq.setHeader('authority', url);
     proxyReq.setHeader('referer', `https://${url}`);
 });
 
-proxy.on("error", (err, req, res) => {
+proxy.on('error', (err, req, res) => {
     console.warn(err);
 });
 
 app.use(cors(corsOptions));
+app.use(fileUpload());
+app.use('/files', express.static('files'));
 app.use(require('morgan')('dev', {
     skip: (req, res) => {
-        return (req.url != "/" && req.url != '/do');
+        return (req.url != "/" && req.url != '/do' && req.url != '/upload');
     }
 }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/do', (req, res) => {
-    const mainUrl = `https://US6-word-view.officeapps.live.com/wv/wordviewerframe.aspx?embed=1&ui=en%2DUS&rs=en%2DUS&WOPISrc=http%3A%2F%2Fus6%2Dview%2Dwopi%2Ewopi%2Elive%2Enet%3A808%2Foh%2Fwopi%2Ffiles%2F%40%2FwFileId%3FwFileId%3D${encodeURIComponent(req.query.url)}&access_token_ttl=0`
+    const mainUrl = `https://US6-word-view.officeapps.live.com/wv/wordviewerframe.aspx?embed=1&ui=en%2DUS&rs=en%2DUS&WOPISrc=http%3A%2F%2Fus6%2Dview%2Dwopi%2Ewopi%2Elive%2Enet%3A808%2Foh%2Fwopi%2Ffiles%2F%40%2FwFileId%3FwFileId%3D${encodeURIComponent(req.query.url)}&access_token_ttl=0`;
     console.info(`Sending request to URL: ${mainUrl}`);
     request(mainUrl).pipe(res);
 });
 
-app.post('/upload')
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    const uploadedFile = req.files.document;
+    const uploadUrl = `${uuidv4()}/${uploadedFile.name}`;
+
+    uploadedFile.mv(`files/${uploadUrl}`, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.send(uploadUrl);
+    });
+});
 
 app.all('*', (req, res) => {
-    const proxyUrl = "https://US6-word-view.officeapps.live.com/wv/"
+    const proxyUrl = 'https://US6-word-view.officeapps.live.com/wv/';
 
     proxy.web(req, res, {
         target: proxyUrl,
         changeOrigin: true
-    })
-})
+    });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

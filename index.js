2d51df52eab6f37649d98f91d88add8283094910
Samyak Jain <samtan106@gmail.com>
2020-04-07 14:08:22 +0530

    Added decoding of proxy url

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.get('/upload_ui', asyncHandler(async(req, res) => {
 }));
 
 app.all('/:docUrl/*', asyncHandler(async(req, res) => {
-    const proxyUrl = req.param.docUrl;
+    const proxyUrl = decodeURI(req.param.docUrl);
     console.log(proxyUrl);
 
     proxy.web(req, res, {
